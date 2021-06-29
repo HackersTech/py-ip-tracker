dec552d52ba1d60c98e0fcbf0388c3496a1aa64a
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-06-29 16:17:31 +0530

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,26 @@
+#
+import os
+os.system('bash ip.sh')
+import pygeoip
+gi  =  pygeoip.GeoIP('/opt/GeoIP/Geo.dat') 
+
+print ("\033[5;35m made by Hackers Tech ")
+
+print ('example -192.168.50.229')
+ip=input("\033[5;36m enter ip address here :")
+if ip !="":
+	def printRecord(ip):
+		rec  =  gi.record_by_name(ip)
+		city  =  rec['city']
+		region  =  rec['region_name']
+		country  =  rec['country_name']
+		long  =  rec['longitude']
+		lat  =  rec['latitude']
+		print ('\033[5;32m [*] Target: '  +  ip  +  ' Geo-located. ')
+		print ('\033[5;33m [+] Latitude: '+str(lat)+  ' Longitude: '+  str(long))
+
+	printRecord(ip)
+
+else :
+	print ("\033[5;31m invalid input exiting !!!")
+
---
+++ ip.sh
@@ -0,0 +1,6 @@
+#
+echo -e "\e[5;33m instagram id -- hackers__tech \e[0m"
+bash i.sh | lolcat -s 2
+
+
+		
